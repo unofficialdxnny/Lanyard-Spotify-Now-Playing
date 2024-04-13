63ccf14d839eb5f58e06d04c53011b5252bcfd80
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2024-04-13 02:16:06 +0100

    TEST FETCH

--- Spotify-Now-Playing/Assets/JS/index.js
+++ Spotify-Now-Playing/Assets/JS/index.js
@@ -1,13 +1,27 @@
 
+  
 function submit() {
     const userID = document.getElementById("userID").value;
 
     if (userID.trim() !== "") {
         localStorage.setItem("userID", userID)
-        alert("UserID Saved Successfully")
+        alert("Success")
     } else {
         alert("Please Enter Discord UserID")
     }
 
 
 }
+
+if (localStorage.getItem("userID") !== null) {
+
+    discorduserID = localStorage.getItem("userID").value;
+    fetch(`https://api.lanyard.rest/v1/users/${discorduserID}`) 
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();  // assuming the API returns JSON
+  })
+}
+
